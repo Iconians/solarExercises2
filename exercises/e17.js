1cbd928897d92c9d16912b600ef531d03405aa26
Iconians <claytoncripe@gmail.com>
2023-01-02 09:38:03 -0800

    having all invididual tests passing

--- exercises/e17.js
+++ exercises/e17.js
@@ -1,3 +1,29 @@
-export function minBy(array, cb) {}
+export function minBy(array, cb) {
+  let temp = array[0]
+  if (array.length) {
+    for (let element of array) {
+      if (cb(temp) > cb(element)) {
+        temp = element
+        console.log(temp);
+      }
+    }
+    return temp
+  }else{
+    return undefined
+  }
+}
 
-export function maxBy(array, cb) {}
+export function maxBy(array, cb) {
+  let temp = array[0]
+  if (array.length) {
+    for (let element of array) {
+      if (cb(temp) < cb(element)) {
+        temp = element
+        console.log(temp);
+      }
+    }
+    return temp
+  }else{
+    return undefined
+  }
+}
